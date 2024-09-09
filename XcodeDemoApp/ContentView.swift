8405712198de1e3378832b10d4a89fa16245c94a
Yousefdawood1 <Yousef.dawood1@outlook.com>
2024-09-09 15:02:08 +0300

    Initial Commit

---
+++ XcodeDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XcodeDemoApp
+//
+//  Created by Yousef Dawood on 09/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XcodeDemoApp/XcodeDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeDemoAppApp.swift
+//  XcodeDemoApp
+//
+//  Created by Yousef Dawood on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct XcodeDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
